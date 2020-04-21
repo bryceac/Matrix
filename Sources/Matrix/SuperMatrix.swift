426854ea7d37061ffa73bedbfde26873cb72efed
Bryce <tonyhawk2100@gmail.com>
2020-04-20 17:52:05 -0700

    override load for subclass.

--- Sources/Matrix/SuperMatrix.swift
+++ Sources/Matrix/SuperMatrix.swift
@@ -17,6 +17,24 @@ import Foundation
  ~~~
  */
 public class SuperMatrix<T: Codable, Element: Codable>: Matrix<T> {
+    
+    /**
+    class method to load Super Matrices from JSON.
+    - parameter path: file path for JSON
+    - Returns: nil if file could not be read or JSON can be parsed.
+    - Note: Like the Matrix class implementation, this method is not to be used on remote files, due to how data is grabbed.
+    */
+    override public class func load(from path: URL) -> SuperMatrix<T, Element>? {
+        
+        // create a decoder object
+        let JSON_DECODER = JSONDecoder()
+        
+        // attempt to parse JSON from file
+        guard let JSON_DATA = Data(contentsOf: path), let DECODED_SUPER_MATRIX = try? JSON_DECODER.decode(SuperMatrix<T, Element>.self, from: JSON_DATA) else { return nil }
+
+        // return decoded SuperMatrix
+        return DECODED_SUPER_MATRIX
+    }
 }
 
 // extension that adds functionality only if the given type is itself a matrix
