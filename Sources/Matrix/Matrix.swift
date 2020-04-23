a05b57179d9cc7b2a86b0b6eee0c2beebc36c2dd
Bryce <tonyhawk2100@gmail.com>
2020-04-22 20:07:40 -0700

    remove boolean check.

--- Sources/Matrix/Matrix.swift
+++ Sources/Matrix/Matrix.swift
@@ -314,8 +314,7 @@ extension Matrix: RandomAccessCollection, MutableCollection {
                         index.column = 0
                     }
                 }
-
-                print("index is less than endIndex: \(index < endIndex)")
+                
                 // make sure index does not go beyond endIndex
                 index = index > endIndex ? endIndex : index
             case -1:
